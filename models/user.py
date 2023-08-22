9ce522d722af21601d5af3caf062f66a3d128770
Favourokerri <favourokerri767@gmail.com>
2023-08-22 23:22:16 +0000

    user

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
+class User(Base, BaseModel):
     """This is the class for user
     Attributes:
         email: email address
