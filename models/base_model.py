1d5c905bad5dff43742cc32bff08a8b990d5ae32
Favourokerri <favourokerri767@gmail.com>
2023-08-22 16:02:39 +0000

    filestorage

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             for key, value in kwargs.items():
-                setattr(key, value)
+                setattr(self, key, value)
 
             del kwargs['__class__']
             self.__dict__.update(kwargs)
