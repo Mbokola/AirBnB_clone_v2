6068d7a3a61dbbb0db2a9ad3c88994078c9bc33b
Favourokerri <favourokerri767@gmail.com>
2023-08-23 00:08:40 +0000

    place model

--- models/place.py
+++ models/place.py
@@ -11,6 +11,7 @@ from models.amenity import Amenity
 from models.review import Review
 from os import getenv
 
+
 class Place(BaseModel, Base):
     """This is the class for Place
     """
@@ -27,3 +28,15 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete-orphan')
+        amenities = relationship('Amenity',
+                                 secondary='place_amenity',
+                                 backref='places', viewonly=False)
+    else:
+        @property
+        def reviews(self):
+            """Getter attribute in case of file storage"""
+            return [review for review in models.storage.all(Review)
+                    if review.place_id == self.id]
