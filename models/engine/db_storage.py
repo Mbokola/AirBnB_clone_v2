5ed6f54d4190911e852cbe7700a15ac6a281fb61
Mbokola <tonypeaks@gmail.com>
2023-08-23 09:13:29 +0000

    db storage add classes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -42,7 +42,7 @@ class db_storage:
         if cls:
             classes = [cls]
         else:
-            classes = [State, City]
+            classes = [State, City, User, Review, Place, Amenity]
 
         objs = {}
         for cls in classes:
