8cc7ead7f3b5177b34f43574b3b22e57164e6201
Mbokola <tonypeaks@gmail.com>
2023-09-09 13:49:43 +0000

    Include do_pack

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
 """ 2-do_deploy_web_static """
 
-from fabric.api import env, put, sudo
+from fabric.api import env, put, sudo, local
 from os.path import exists
-do_pack = __import__('1-pack_web_static').do_pack
+from datetime import datetime
 
 env.hosts = ['100.26.231.64', '107.23.16.147']
+
+
+def do_pack():
+    """
+    Create a .tgz archive from the web_static folder.
+    """
+    try:
+        # Create the 'versions' directory if it doesn't exist
+        local("mkdir -p versions")
+
+        # Generate the archive name using the current date and time
+        now = datetime.now()
+        archive_name = f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
+
+        # Use the tar command to create the .tgz archive
+        local(f"tar -czvf versions/{archive_name} web_static")
+
+        # Return the archive path if successful
+        return f"versions/{archive_name}"
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
