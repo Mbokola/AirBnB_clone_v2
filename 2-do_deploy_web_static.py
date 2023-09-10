f65ec847b671a06c468e9ced7e8a7db60f950075
Mbokola <tonypeaks@gmail.com>
2023-09-10 10:07:56 +0000

    Trial

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@
 from fabric.api import env, put, sudo, local
 from os.path import exists
 
-env.hosts = ['100.26.231.64', '107.23.16.147', 'localhost']
+env.hosts = ['100.26.231.64', '107.23.16.147', '127.0.0.1']
 
 
 def do_deploy(archive_path):
