b78e2c2f060003f8303c9417fc6c8db3ecc37c09
Mbokola <tonypeaks@gmail.com>
2023-09-24 11:09:33 +0000

    Task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" 3-python_route module """
+
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ display “Hello HBNB!” """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ display “HBNB” """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def variable(text):
+    """  display “C ” followed by the value of the text variable """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """  display “Python ” followed by the value of the text variable """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    if n.isnumeric():
+        return f"{n} is a number"
+    else:
+        abort(404)
+
+
+@app.route("/number_template/<n>", strict_slashes=False)
+def number_template(n):
+    if n.isnumeric():
+        return render_template('5-number.html', heading=f'Number: {n}')
+    else:
+        abort(404)
+
+
+@app.route("/number_odd_or_even/<n>", strict_slashes=False)
+def number_odd_even(n):
+    if n.isnumeric():
+        num = int(n)
+        parity = "odd" if num % 2 else "even"
+        return render_template('6-number_odd_or_even.html', heading=f'Number: \
+{n} is {parity}')
+    else:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
